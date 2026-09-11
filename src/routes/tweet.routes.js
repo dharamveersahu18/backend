4dd577b0f9d8e66225989288a696784977925712
Dharam <dharamveersahu18@gmail.com>
2026-09-11 07:11:32 +0530

    feat: add routes for playlists, subscriptions, tweets, and videos with authentication middleware

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
+import { verifyJWT, verifyJWTOptional } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/").post(verifyJWT, createTweet);
+router.route("/user/:userId").get(verifyJWTOptional, getUserTweets);
+router.route("/:tweetId").patch(verifyJWT, updateTweet).delete(verifyJWT, deleteTweet);
+
+export default router;
---
+++ src/routes/video.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import {
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+} from "../controllers/video.controller.js";
+import { verifyJWT, verifyJWTOptional } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+router
+  .route("/")
+  .get(verifyJWTOptional, getAllVideos)
+  .post(
+    verifyJWT,
+    upload.fields([
+      { name: "videoFile", maxCount: 1 },
+      { name: "thumbnail", maxCount: 1 },
+    ]),
+    publishAVideo
+  );
+
+router
+  .route("/:videoId")
+  .get(verifyJWTOptional, getVideoById)
+  .delete(verifyJWT, deleteVideo)
+  .patch(verifyJWT, upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus);
+
+export default router;
